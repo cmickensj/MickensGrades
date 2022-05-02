654b83f227acb90a22a127beb81d9791f36fc0ad
cmickensj <89092260+cmickensj@users.noreply.github.com>
2022-05-01 22:40:55 -0500

    Add files via upload

---
+++ MickensGrades.js
@@ -0,0 +1,14 @@
+// the student's grade will show in the console
+
+var points_possible = 50;
+
+let
+number = prompt('how many did you miss?');
+
+if (number === 13){
+    console.log('Score 74');
+} else if (number === 25){
+    console.log ('Score: 50% (ouch 😬)');
+}
+
+
